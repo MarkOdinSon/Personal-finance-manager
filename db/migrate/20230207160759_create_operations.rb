9a2ab8aa95467cafc0d311ff81a4993fece146f2
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-02-07 18:14:37 +0200

    init commit 2th

---
+++ db/migrate/20230207160759_create_operations.rb
@@ -0,0 +1,13 @@
+class CreateOperations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :operations do |t|
+      t.string :otype
+      t.decimal :amount
+      t.datetime :odate
+      t.string :description
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
