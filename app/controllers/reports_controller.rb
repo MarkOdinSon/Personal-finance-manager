b0e52a7cc59263ed86fad1412dd9d22c9ac86312
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-02-13 11:31:06 +0200

    reports page in progress #2

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,10 +6,14 @@ class ReportsController < ApplicationController
   end
 
   def report_by_category
+    @pageTitle = 'Report by category'
+
+    @array_of_category = Category.limit(10).pluck(:name)
 
   end
 
   def report_by_dates
+    @pageTitle = 'Report by dates'
 
   end
 end
