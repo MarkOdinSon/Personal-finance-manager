6822a9ec15071206b85253803d65222d0c690174
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-02-15 19:49:10 +0200

    seeds done only tests need to be done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,60 +2,80 @@ require 'faker'
 
 # This is not related to categories but I need to add a lot of entries to demonstrate search and pagination
 
-50.times do
-  begin
-    Category.create!([{
-                        name: Faker::Food.dish,
-                        description: Faker::Alphanumeric.alpha(number: 10)
-                      }
-                     ])
-  rescue
-
-  end
-
-end
-
-50.times do
-  begin
-    Category.create!([{
-                        name: Faker::Tea.variety,
-                        description: Faker::Alphanumeric.alpha(number: 10)
-                      }
-                     ])
-  rescue
-
-  end
-
-end
-
-100.times do
-  begin
-    Category.create!([{
-                        name: Faker::Dessert.variety,
-                        description: Faker::Alphanumeric.alpha(number: 10)
-                      }
-                     ])
-  rescue
-
-  end
-
-end
+Category.create!([{
+                    name: 'Vehicle (Fuel)',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Vehicle (Service)',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Medications',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Massage',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Shoes',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Restaurants',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Payment of bills',
+                    description: "Light (power, electricity), gas, water"
+                  },
+                  {
+                    name: 'Internet',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Mobile operator services',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Household goods',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Nutrition (food)',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Gifts',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Subway pass',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Computer games',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  },
+                  {
+                    name: 'Drinking water',
+                    description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}"
+                  }])
 
 puts "Created #{Category.count} Categories"
 
-100.times do
+130.times do
   begin
     Operation.create!([{
-                        amount: rand(5.0...10000.0).round(2),
-                        odate: Faker::Date.between(from: '2019-06-01', to: '2023-01-01'),
-                        description: Faker::Alphanumeric.alpha(number: 10),
+                        amount: rand(50.0...10000.0).round(2),
+                        odate: Faker::Date.between(from: '2019-01-01', to: '2023-01-01'),
+                        description: "Here same description: #{Faker::Alphanumeric.alpha(number: 10)}",
                         category: Category.offset(rand(Category.count)).first
                       }
                      ])
   rescue
-
   end
-
 end
 
 puts "Created #{Operation.count} Operations"
